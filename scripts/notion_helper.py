0800879f36bbf992da6f124167940562e2f8a10f
lengruohanqiu2 <159115519+lengruohanqiu2@users.noreply.github.com>
2024-05-01 18:19:01 +0800

    Update notion_helper.py

--- scripts/notion_helper.py
+++ scripts/notion_helper.py
@@ -43,6 +43,7 @@ class NotionHelper:
     }
     database_id_dict = {}
     image_dict = {}
+    heatmap_block_id = None
     def __init__(self):
         self.client = Client(auth=os.getenv("NOTION_TOKEN"), log_level=logging.ERROR)
         self.__cache = {}
@@ -102,12 +103,12 @@ class NotionHelper:
             # 检查子块的类型
 
             if child["type"] == "child_database":
-                self.database_id_dict[
-                    child.get("child_database").get("title")
-                ] = child.get("id")
-            elif child["type"] == "image" and child.get("image").get("external"):
-                self.image_dict["url"] = child.get("image").get("external").get("url")
-                self.image_dict["id"] = child.get("id")
+                self.database_id_dict[child.get("child_database").get("title")] = (
+                    child.get("id")
+                )
+            elif child["type"] == "embed" and child.get("embed").get("url"):
+                if child.get("embed").get("url").startswith("https://heatmap.malinkang.com/"):
+                    self.heatmap_block_id = child.get("id")
             # 如果子块有子块，递归调用函数
             if "has_children" in child and child["has_children"]:
                 self.search_database(child["id"])
@@ -149,11 +150,9 @@ class NotionHelper:
         if len(update_properties) > 0:
             self.client.databases.update(database_id=id, properties=update_properties)
 
-    def update_image_block_link(self, block_id, new_image_url):
+    def update_heatmap(self, block_id, url):
         # 更新 image block 的链接
-        self.client.blocks.update(
-            block_id=block_id, image={"external": {"url": new_image_url}}
-        )
+        return self.client.blocks.update(block_id=block_id, embed={"url": url})
 
     def get_week_relation_id(self, date):
         year = date.isocalendar().year
